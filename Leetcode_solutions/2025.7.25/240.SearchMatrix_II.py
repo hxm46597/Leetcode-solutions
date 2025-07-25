dc68f2e20d9b321bccd28416c539f0f368588e60
kennywin <hxm613@outlook.com>
2025-07-25 22:56:26 +0800

    Greedy Algorithm

---
+++ Leetcode_solutions/2025.7.25/240.SearchMatrix_II.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        i , j = len(matrix) - 1 , 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target:
+                i -= 1
+            elif matrix[i][j] < target:
+                j += 1
+            else:
+                return True
+        return False
+sol = Solution()
+matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+target = 5
+result = sol.searchMatrix(matrix,target)
+print(result)
