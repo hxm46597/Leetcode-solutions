e5c8d1a3f03144efe461a2caba3e8e17e97875fb
kennywin <hxm613@outlook.com>
2025-07-25 20:48:25 +0800

    dynamic programing

---
+++ Leetcode_solutions/2025.7.25/264.UglyNumber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res, a, b, c = [1] * n, 0, 0, 0
+        for i in range(1, n):
+            n2, n3, n5 = res[a] * 2, res[b] * 3, res[c] * 5
+            res[i] = min(n2,n3,n5)
+            if res[i] == n2: a += 1
+            if res[i] == n3: b += 1
+            if res[i] == n5: c += 1
+        return res[-1]
+sol = Solution()
+n = 10
+result = sol.nthUglyNumber(n)
+print(result)
