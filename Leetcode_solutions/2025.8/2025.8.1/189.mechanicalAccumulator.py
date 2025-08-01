379afeb333ba22ca66c6fe437eefa1e32bc93066
kennywin <hxm613@outlook.com>
2025-08-01 11:41:55 +0800

    Recursion to get the dum of the first n terms

---
+++ Leetcode_solutions/2025.8/2025.8.1/189.mechanicalAccumulator.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def __init__(self):
+        self.res = 0
+
+    def mechanicalAccumulator(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        target > 1 and self.mechanicalAccumulator(target - 1)
+        self.res += target
+        return self.res
+
+target = 5
+sol = Solution()
+res = sol.mechanicalAccumulator(target)
+print(res)
