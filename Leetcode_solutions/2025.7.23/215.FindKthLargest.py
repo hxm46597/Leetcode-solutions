2fd1526b1bfca8bc4e80f79a2a8a1b314b0de96f
kennywin <hxm613@outlook.com>
2025-08-01 08:41:35 +0800

    change the content

--- Leetcode_solutions/2025.7.23/215.FindKthLargest.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        return sorted(nums)[len(nums) - k]
-sol = Solution()
-nums = [3,2,3,1,2,4,5,5,6]
-k = 4
-result = sol.findKthLargest(nums,k)
-print(result)
