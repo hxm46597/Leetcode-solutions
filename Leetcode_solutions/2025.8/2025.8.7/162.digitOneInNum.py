bf22ea48b5cde087b1cf6de24ee0875a5be9b6d4
kennywin <hxm613@outlook.com>
2025-08-07 09:52:57 +0800

    Count the amount of "1" in a number list

---
+++ Leetcode_solutions/2025.8/2025.8.7/162.digitOneInNum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def digitOneInNumber(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        digit, res = 1, 0
+        high, cur, low = num // 10, num % 10, 0
+        while high != 0 or cur != 0:
+            if cur == 0: res += high * digit
+            elif cur == 1: res += high * digit + low + 1
+            else: res += (high + 1) * digit
+            low += cur * digit
+            cur = high % 10
+            high //= 10
+            digit *= 10
+        return res
+sol = Solution()
+num = 13
+res = sol.digitOneInNumber(num)
+print(res)
